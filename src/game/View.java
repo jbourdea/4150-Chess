9b1fb44dd1d235f0c1d95a65a5d0c880c4f200cf
Matt Stark <mattrstark@gmail.com>
2014-10-19 16:17:47 -0400

    Changed View to Allow for more spacing.
    Looks less cluttered this way. If you guys want it changed back, let me
    know.

--- src/game/View.java
+++ src/game/View.java
@@ -15,10 +15,10 @@ public class View {
 	
 	public void DisplayBoard(Board gameBoard) {
 		
-		System.out.print("   A B C D E F G H" + "\n");
+		System.out.print("    A B C D E F G H" + "\n\n");
 		
 		for(int y = 0; y < gameBoard.height; y++) {
-			System.out.print(y + "  ");
+			System.out.print(y + "   ");
 			for(int x = 0; x < gameBoard.width; x++) {
 				if (gameBoard.tiles[x][y].piece == null) {
 					System.out.print("- ");
@@ -27,10 +27,10 @@ public class View {
 					System.out.print(gameBoard.tiles[x][y].piece.toString() + " ");
 				}
 			}
-			System.out.print(" " + y + "\n");
+			System.out.print("  " + y + "\n");
 		}
 		
-		System.out.print("   A B C D E F G H" + "\n" );
+		System.out.print("\n    A B C D E F G H" + "\n" );
 		
 	}
 	
