e0b357f612706b975520ef191463e9ea999d815f
spood <spood@192.168.1.106>
2014-10-19 23:02:13 -0400

    added comment for the MoveCompleteResult class

--- src/game/MoveCompleteResult.java
+++ src/game/MoveCompleteResult.java
@@ -1,5 +1,10 @@
 package game;
 
+/**
+ * Container class for the result of a game state. Specifying if the game is ended or not. If the game is ended, 
+ * and no winner is provided it is assumed to be a stalemate.
+ *
+ */
 public class MoveCompleteResult {
 	public boolean gameOver;
 	public Player winner;
