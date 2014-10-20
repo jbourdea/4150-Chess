b76fd53768d901593ad00d3d3f3ff7cab7ac245f
spood <spood@192.168.1.106>
2014-10-19 23:34:23 -0400

    Added comments for Piece

--- src/game/Piece.java
+++ src/game/Piece.java
@@ -1,5 +1,9 @@
 package game;
 
+/**
+ * Generic Piece class that is extended by each individual game piece (Bishop, King, Knight, Pawn, Queen, Rook).
+ * Contains a reference to the player and a variable for if it has moved.
+ */
 public class Piece {
 	
 	public Player owner = null;
