16460b003356f8ab4d9e9e1ade1b57a83d8e82a0
spood <spood@192.168.1.106>
2014-10-19 23:31:50 -0400

    comments for Player

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,7 +1,11 @@
 package game;
 
+/**
+ * Container that has a reference to the player's color and opponent
+ */
 public class Player {
 	
+	// "White" or "Black"
 	public String color;
 	public Player opponent = null;
 	
